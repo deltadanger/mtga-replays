388c521ba2e3e55d011c57a4729c20b324caff13
thierryjacoel <delta.danger@gmail.com>
2020-03-16 16:54:18 +0100

    Added parser for log files

---
+++ api/management/commands/test_parse.py
@@ -0,0 +1,25 @@
+import logging
+import os
+
+from django.core.management import BaseCommand
+
+from api.helpers.parser import MtgaLogParser
+
+
+logger = logging.getLogger(__name__)
+
+
+class Command(BaseCommand):
+
+    # def add_arguments(self, parser):
+    #     parser.add_argument('--dry-run', action='store_true', help='List the requests, do not perform the update.')
+
+    def handle(self, *args, **options):
+        for path, dirs, files in os.walk("H:\Magic The Gathering Arena\MTGA\MTGA_Data\Logs\Logs"):
+            for file in files:
+                full_path = os.path.join(path, file)
+                logger.info(file)
+                parser = MtgaLogParser(full_path)
+                parser.fetch_infos()
+                logger.info(parser.games)
+                logger.info(len(parser.games))
--- api/views.py
+++ api/views.py
@@ -1,8 +1,12 @@
 from django.http import JsonResponse
-from django.shortcuts import render
 from rest_framework.views import APIView
+
+from api.helpers.parser import MtgaLogParser
 
 
 class BoardView(APIView):
     def get(self, request):
+        parser = MtgaLogParser('UTC_Log - 03-15-2020 20.19.00.log')
+        parser.fetch_infos()
+        print(parser.games)
         return JsonResponse({'blah': 'asd'})
